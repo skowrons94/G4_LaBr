5ac5752dc7aecaa6a4bce8bef9766a26aeb735b0
Jakub Skowronski <jakub.skowronski@protonmail.com>
2024-05-12 11:32:07 +0200

    Update

--- Run_Beamspot.py
+++ Run_Beamspot.py
@@ -9,14 +9,14 @@ file = sys.argv[1]
 x, y, z, d = 0, 0, 0, 7
 
 # Create a histo for x and y
-xbins = np.arange(-3, 3.1, 0.3)
 ybins = np.arange(-3, 3.1, 0.3)
+zbins = np.arange(-3, 3.1, 0.3)
 
-for x in xbins:
+for y in ybins:
     
-    for y in ybins:
+    for z in zbins:
 
-        x, y = round(x, 1), round(y, 1) 
+        y, z = round(y, 1), round(z, 1) 
 
         # Wait for 2 seconds
         subprocess.run(["sleep", "2"])
